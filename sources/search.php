87e1c3ed12a045d256e54b1a5e54defe8e577491
minh tri <minhtri.blueweb@gmail.com>
2025-08-25 22:47:08 +0700

    em co pauuu

--- sources/search.php
+++ sources/search.php
@@ -3,7 +3,7 @@ if (!defined('SOURCES')) die("Error");
 
 @$keyword = htmlspecialchars($_GET['keyword']);
 $curPage =  max(1, isset($_GET['page']) ? (int)$_GET['page'] : 1);
-$perPage = 1;
+$perPage = 20;
 $options = ['table' => 'tbl_product', 'status' => 'hienthi', 'select' => "id, name{$lang}, slug{$lang}, file, regular_price, sale_price, views", 'keyword' => $keyword, 'pagination' => [$perPage, $curPage]];
 $total = $fn->count_data($options);
 $product = $fn->show_data($options);
